78dd51fec163fcf936b4e9691e2cf7af6e676c36
danzeroum <danzeroum@gmail.com>
2026-02-04 16:59:58 -0300

    Organizando os arquivos

---
+++ python/buildtovalue/cli/main.py
@@ -0,0 +1,16 @@
+"""BuildToValue CLI entrypoint"""
+import click
+
+@click.group()
+@click.version_option(version="2.2.0")
+def cli():
+    """BuildToValue Sovereign Trust OS CLI"""
+    pass
+
+@cli.command()
+def version():
+    """Show version"""
+    click.echo("BuildToValue v2.2.0")
+
+if __name__ == "__main__":
+    cli()
