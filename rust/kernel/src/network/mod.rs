4a5cdc777b1c48e2d9a99395ba1dc165b3c8801a
danzeroum <danzeroum@gmail.com>
2026-02-18 01:08:40 -0300

    P5

--- rust/kernel/src/network/mod.rs
+++ rust/kernel/src/network/mod.rs
@@ -1,3 +1,8 @@
 //! Network Module v1.7.0 (ADR-014)
 pub mod ip_classifier;
+pub mod jurisdiction;
+
 pub use ip_classifier::{IpClassifier, IpClassification, IpRisk, IpCategory};
+pub use jurisdiction::{
+    JurisdictionMapper, JurisdictionResult, Jurisdiction, ComplianceFramework,
+};
